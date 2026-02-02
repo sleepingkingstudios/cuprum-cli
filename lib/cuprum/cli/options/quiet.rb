d0127bd162cc1b0c97c5a24668df6d71a96c9681
Rob Smith <sleepingkingstudios@gmail.com>
2026-02-02 11:02:10 -0500

    Implement Options::Quiet.

---
+++ lib/cuprum/cli/options/quiet.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'cuprum/cli/options'
+
+module Cuprum::Cli::Options
+  # Defines --quiet option, which silences non-essential output.
+  module Quiet
+    DESCRIPTION = 'Silences non-essential console outputs.'
+    private_constant :DESCRIPTION
+
+    class << self
+      private
+
+      def included(other)
+        super
+
+        other.option   :quiet,
+          type:        :boolean,
+          aliases:     :q,
+          default:     false,
+          description: DESCRIPTION
+      end
+    end
+
+    # (see Cuprum::Cli::Dependencies::StandardIo::Helpers#say)
+    def say(message, quiet: false, **)
+      return if options.fetch(:quiet, false) && !quiet
+
+      # byebug
+
+      super
+    end
+  end
+end
