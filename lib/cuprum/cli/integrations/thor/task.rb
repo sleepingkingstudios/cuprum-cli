3c7d890b1019ccfed345e8e00182c2c75f9ffedf
Rob Smith <sleepingkingstudios@gmail.com>
2026-02-18 18:00:43 -0500

    Implement Thor::Task::Builder.

--- lib/cuprum/cli/integrations/thor/task.rb
+++ lib/cuprum/cli/integrations/thor/task.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
 require 'sleeping_king_studios/tools/toolbelt'
 require 'sleeping_king_studios/tools/toolbox/subclass'
 require 'thor'
@@ -10,6 +12,152 @@ module Cuprum::Cli::Integrations::Thor
   # Thor task wrapping a Cuprum::Cli command.
   class Task < ::Thor
     extend SleepingKingStudios::Tools::Toolbox::Subclass
+
+    # Generates a Thor::Task wrapping a Cuprum::Cli command class.
+    class Builder
+      extend Forwardable
+
+      NUMERIC_TYPES = Set.new(%w[big_decimal integer float]).freeze
+      private_constant :NUMERIC_TYPES
+
+      # @param command_class [Class] the command to execute.
+      def initialize(command_class)
+        validate_command_class(command_class)
+
+        @command_class = command_class
+      end
+
+      # @return [Class] the command to execute.
+      attr_reader :command_class
+
+      def_delegators :@command_class,
+        :arguments,
+        :description,
+        :full_description,
+        :full_description?,
+        :short_name
+
+      # Generates a Thor::Task wrapping the command class.
+      #
+      # The generated task will be assigned Thor metadata automatically, based
+      # on the configuration of the command class.
+      #
+      # @return [Class] the generated Task class.
+      def build
+        Cuprum::Cli::Integrations::Thor::Task
+          .subclass(command_class)
+          .tap do |task|
+            apply_metadata(task)
+
+            task.alias_method command_class.short_name, :call_command
+          end
+      end
+
+      private
+
+      def apply_arguments(task)
+        command_class.arguments.each do |argument|
+          params = {
+            banner:   argument.parameter_name || argument.name.to_s.upcase,
+            desc:     argument.description,
+            optional: !argument.required?,
+            type:     parameter_type(argument)
+          }
+
+          task.argument(argument.name, **params)
+        end
+      end
+
+      def apply_options(task)
+        command_class.options.each_value do |option|
+          params = {
+            aliases:  option.aliases,
+            banner:   option.parameter_name || option.name.to_s.upcase,
+            desc:     option.description,
+            required: option.required?,
+            type:     parameter_type(option)
+          }
+
+          task.option(option.name, **params)
+        end
+      end
+
+      def apply_metadata(task)
+        task.namespace(namespace)
+        task.desc(signature, description)
+
+        task.long_desc(full_description) if full_description?
+
+        apply_arguments(task)
+        apply_options(task)
+
+        task
+      end
+
+      def argument_signature(argument)
+        signature = argument.parameter_name || argument.name.to_s.upcase
+
+        argument.variadic ? " ...#{signature}" : " #{signature}"
+      end
+
+      def arguments_signature
+        arguments.map { |argument| argument_signature(argument) }.join
+      end
+
+      def namespace
+        command_class.namespace? ? command_class.namespace : 'default'
+      end
+
+      def parameter_type(parameter)
+        if parameter.is_a?(Cuprum::Cli::Argument) && parameter.variadic?
+          return :array
+        end
+
+        type = parameter.type
+        type = type.name if type.is_a?(Class)
+        type = tools.string_tools.underscore(type)
+
+        return :numeric if NUMERIC_TYPES.include?(type)
+
+        type.to_sym
+      end
+
+      def signature
+        "#{short_name}#{arguments_signature}"
+      end
+
+      def tools
+        SleepingKingStudios::Tools::Toolbelt.instance
+      end
+
+      def validate_command_class(command_class, as: 'command_class')
+        tools.assertions.validate_class(command_class, as:)
+        tools.assertions.validate_inherits_from(
+          command_class,
+          as:,
+          expected: Cuprum::Cli::Command
+        )
+
+        validate_command_description(command_class, as:)
+        validate_command_name(command_class, as:)
+      end
+
+      def validate_command_description(command_class, as:)
+        description = command_class.description
+
+        return unless description.nil? || description.empty?
+
+        raise ArgumentError, "#{as} does not have a description"
+      end
+
+      def validate_command_name(command_class, as:)
+        name = command_class.full_name
+
+        return unless name.nil? || name.empty?
+
+        raise ArgumentError, "#{as} does not have a full_name"
+      end
+    end
 
     # Ensures that the task exists with a non-zero status code on a failure.
     #
---
+++ spec/cuprum/cli/integrations/thor/task/builder_spec.rb
@@ -0,0 +1,297 @@
+# frozen_string_literal: true
+
+require 'cuprum/cli/command'
+require 'cuprum/cli/integrations/thor/task'
+
+RSpec.describe Cuprum::Cli::Integrations::Thor::Task::Builder,
+  integration: :thor \
+do
+  subject(:builder) { described_class.new(command_class) }
+
+  let(:description) do
+    'No one is quite sure what this does.'
+  end
+  let(:full_name) { 'spec:custom' }
+  let(:command_class) do
+    Class.new(Cuprum::Cli::Command).tap do |klass|
+      klass.description(description)
+
+      klass.full_name(full_name)
+    end
+  end
+
+  describe '.new' do
+    it { expect(described_class).to be_constructible.with(1).argument }
+
+    describe 'with nil' do
+      let(:error_message) do
+        tools.assertions.error_message_for('class', as: 'command_class')
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(nil) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an Object' do
+      let(:error_message) do
+        tools.assertions.error_message_for('class', as: 'command_class')
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(Object.new.freeze) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an invalid Class' do
+      let(:error_message) do
+        tools.assertions.error_message_for(
+          'inherit_from',
+          as:       'command_class',
+          expected: Cuprum::Cli::Command
+        )
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(Class.new) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an anonymous Class' do
+      let(:error_message) do
+        'command_class does not have a full_name'
+      end
+      let(:command_class) do
+        Class.new(Cuprum::Cli::Command) do
+          description 'No one is quite sure what this does.'
+        end
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(command_class) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a command Class without a description' do
+      let(:error_message) do
+        'command_class does not have a description'
+      end
+      let(:command_class) do
+        Class.new(Cuprum::Cli::Command) do
+          full_name 'spec:custom'
+        end
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(command_class) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+  end
+
+  describe '#build' do
+    let(:task_class)   { builder.build }
+    let(:task)         { task_class.new }
+    let(:thor_command) { task_class.commands[command_class.short_name] }
+
+    it { expect(builder).to respond_to(:build).with(0).arguments }
+
+    it { expect(task_class).to be_a(Class) }
+
+    it { expect(task_class).to be < Cuprum::Cli::Integrations::Thor::Task }
+
+    it { expect(task_class.arguments).to be == [] }
+
+    it { expect(task.command_class).to be command_class }
+
+    it { expect(task_class.namespace).to be == command_class.namespace }
+
+    it 'should define the task method' do
+      expect(task)
+        .to have_aliased_method(:call_command)
+        .as(command_class.short_name)
+    end
+
+    it { expect(thor_command.description).to be == command_class.description }
+
+    it { expect(thor_command.long_description).to be nil }
+
+    it { expect(thor_command.options).to be == {} }
+
+    it { expect(thor_command.usage).to be == command_class.short_name }
+
+    context 'when the command class does not have a namespace' do
+      let(:full_name) { 'do_something' }
+
+      it { expect(task_class.namespace).to be == 'default' }
+
+      it { expect(thor_command.usage).to be == command_class.short_name }
+    end
+
+    context 'when the command class has a full description' do
+      let(:value) do
+        <<~DESC
+          No one is quite sure what this does.
+
+          ...but it sure looks cool!
+        DESC
+      end
+
+      before(:example) { command_class.full_description(value) }
+
+      it { expect(thor_command.long_description).to be == value }
+    end
+
+    context 'when the command class defines arguments' do
+      let(:expected_arguments) do
+        [
+          {
+            banner:      'COLOR',
+            description: nil,
+            name:        'color',
+            required:    true,
+            type:        :numeric
+          },
+          {
+            banner:      '<circle, square, triangle>',
+            description: nil,
+            name:        'shape',
+            required:    false,
+            type:        :string
+          }
+        ]
+      end
+      let(:expected_usage) do
+        "#{command_class.short_name} COLOR <circle, square, triangle>"
+      end
+
+      before(:example) do
+        command_class.argument :color, required: true, type: :integer
+        command_class.argument :shape,
+          parameter_name: '<circle, square, triangle>'
+      end
+
+      it { expect(thor_command.usage).to be == expected_usage }
+
+      it 'should define the arguments', :aggregate_failures do
+        expect(task_class.arguments.size).to be == expected_arguments.size
+
+        task_class.arguments.zip(expected_arguments).each \
+        do |argument, expected|
+          expect(argument).to have_attributes(**expected)
+        end
+      end
+    end
+
+    context 'when the command class defines a variadic argument' do
+      let(:expected_arguments) do
+        [
+          {
+            banner:      'COLOR',
+            description: nil,
+            name:        'color',
+            required:    true,
+            type:        :numeric
+          },
+          {
+            banner:      'SHAPE',
+            description: nil,
+            name:        'shape',
+            required:    false,
+            type:        :string
+          },
+          {
+            banner:      'SIZES',
+            description: nil,
+            name:        'sizes',
+            required:    false,
+            type:        :array
+          },
+          {
+            banner:      'TRANSPARENCY',
+            description: nil,
+            name:        'transparency',
+            required:    false,
+            type:        :string
+          }
+        ]
+      end
+      let(:expected_usage) do
+        "#{command_class.short_name} COLOR SHAPE ...SIZES TRANSPARENCY"
+      end
+
+      before(:example) do
+        command_class.argument :color, required: true, type: :integer
+        command_class.argument :shape
+        command_class.argument :sizes, variadic: true
+        command_class.argument :transparency
+      end
+
+      it { expect(thor_command.usage).to be == expected_usage }
+
+      it 'should define the arguments', :aggregate_failures do
+        expect(task_class.arguments.size).to be == expected_arguments.size
+
+        task_class.arguments.zip(expected_arguments).each \
+        do |argument, expected|
+          expect(argument).to have_attributes(**expected)
+        end
+      end
+    end
+
+    context 'when the command class defines options' do
+      let(:expected_options) do
+        {
+          color:       {
+            aliases:     [],
+            banner:      'COLOR',
+            description: nil,
+            name:        'color',
+            required:    false,
+            type:        :numeric
+          },
+          shape:       {
+            aliases:     [],
+            banner:      '<circle, square, triangle>',
+            description: nil,
+            name:        'shape',
+            required:    true,
+            type:        :string
+          },
+          transparent: {
+            aliases:     %w[-t],
+            banner:      'TRANSPARENT',
+            description: nil,
+            name:        'transparent',
+            required:    false,
+            type:        :boolean
+          }
+        }
+      end
+
+      before(:example) do
+        command_class.option :color, type: :integer
+        command_class.option :shape,
+          required:       true,
+          parameter_name: '<circle, square, triangle>'
+        command_class.option :transparent, type: :boolean, aliases: 't'
+      end
+
+      it 'should define the options', :aggregate_failures do
+        expect(thor_command.options.keys).to be == expected_options.keys
+
+        thor_command.options.each do |name, option|
+          expect(option).to have_attributes(**expected_options[name])
+        end
+      end
+    end
+  end
+
+  describe '#command_class' do
+    include_examples 'should define reader', :command_class
+  end
+end
