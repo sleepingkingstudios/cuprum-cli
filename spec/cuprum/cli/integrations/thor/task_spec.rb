45b00657fe048c760a5523e212cff7518603386f
Rob Smith <sleepingkingstudios@gmail.com>
2026-04-07 16:33:34 -0400

    Ensure Thor tasks exit with non-zero status on failure.

--- spec/cuprum/cli/integrations/thor/task_spec.rb
+++ spec/cuprum/cli/integrations/thor/task_spec.rb
@@ -69,6 +69,66 @@ RSpec.describe Cuprum::Cli::Integrations::Thor::Task, integration: :thor do
 
         expect(mock_io.output_stream.string).to be == expected
       end
+
+      context 'when the command returns a failing result' do
+        let(:command_class) { Spec::FailingCommand }
+
+        example_class 'Spec::FailingCommand', Cuprum::Cli::Command do |klass|
+          klass.dependency :standard_io
+
+          klass.define_method :process do
+            standard_io.write_output "#{self.class} called with no parameters."
+
+            Cuprum::Result.new(status: :failure)
+          end
+        end
+
+        before(:example) { allow(task).to receive(:exit_proxy) } # rubocop:disable RSpec/SubjectStub
+
+        it 'should call the command' do
+          task.call_command
+
+          expect(mock_io.output_stream.string).to be == expected
+        end
+
+        it 'should exit with a non-zero status code' do
+          task.call_command
+
+          expect(task).to have_received(:exit_proxy).with(false) # rubocop:disable RSpec/SubjectStub
+        end
+      end
+
+      context 'when the command returns a failing result with an error' do
+        let(:command_class) { Spec::FailingCommand }
+
+        example_class 'Spec::FailingCommand', Cuprum::Cli::Command do |klass|
+          klass.dependency :standard_io
+
+          klass.define_method :process do
+            standard_io.write_output "#{self.class} called with no parameters."
+
+            error = Cuprum::Error.new(message: 'Something went wrong.')
+
+            Cuprum::Result.new(error:)
+          end
+        end
+
+        before(:example) { allow(task).to receive(:exit_proxy) } # rubocop:disable RSpec/SubjectStub
+
+        it 'should call the command' do
+          task.call_command
+
+          expect(mock_io.output_stream.string).to be == expected
+        end
+
+        it 'should exit with a non-zero status code' do
+          task.call_command
+
+          expect(task) # rubocop:disable RSpec/SubjectStub
+            .to have_received(:exit_proxy)
+            .with('Something went wrong.')
+        end
+      end
     end
 
     context 'when the task is initialized with arguments' do
